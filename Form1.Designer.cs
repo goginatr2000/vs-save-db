0876442492c9b2e6bfb3e9f29ce929896f932854
George <gogicsgo2@gmail.com>
2023-12-14 02:06:06 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Save_DB_Gorbachev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnBackup.FlatAppearance.BorderSize = 0;
+            this.btnBackup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBackup.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBackup.ForeColor = System.Drawing.Color.Black;
+            this.btnBackup.Location = new System.Drawing.Point(12, 12);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(193, 51);
+            this.btnBackup.TabIndex = 0;
+            this.btnBackup.Text = "СОХРАНЕНИЕ БД";
+            this.btnBackup.UseVisualStyleBackColor = false;
+            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(217, 75);
+            this.Controls.Add(this.btnBackup);
+            this.Name = "Form1";
+            this.Text = "Save";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBackup;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Save_DB_Gorbachev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBackup_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                // Параметры подключения к базе данных
+                string connectionString = "Data Source=(local);Initial Catalog=GORBACHEV;Integrated Security=True";
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectionString);
+                string databaseName = builder.InitialCatalog;
+                string serverName = builder.DataSource;
+
+                // Создание пути для сохранения резервной копии
+                string backupDirectory = @"C:\Users\georg\OneDrive\Рабочий стол\Fract2K\DB"; // Укажите путь к папке, где будет сохраняться резервная копия
+                string backupFileName = $"{serverName}_{databaseName}_{DateTime.Now:yyyyMMddHHmmss}.bak";
+                string backupFilePath = System.IO.Path.Combine(backupDirectory, backupFileName);
+
+                // Создание соединения с базой данных
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Создание команды для создания резервной копии
+                    using (SqlCommand command = new SqlCommand($"BACKUP DATABASE [{databaseName}] TO DISK='{backupFilePath}' WITH FORMAT", connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                MessageBox.Show("Резервная копия базы данных успешно создана.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при создании резервной копии базы данных: {ex.Message}");
+            }
+        }
+    }
+
+
+            
+        
+    
+}
+
+    
